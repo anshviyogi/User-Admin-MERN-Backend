2b0750bf57b1c3377782e5f08a2f3ea782b7513f
AnshViyogi <auni.viyogi@gmail.com>
2022-09-29 00:28:28 +0530

    assignment

--- model/db.js
+++ model/db.js
@@ -1,12 +1,27 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI||'mongodb+srv://ansh:anshviyogi@cluster0.syyc1cg.mongodb.net/?retryWrites=true&w=majority',{
-    useNewUrlParser:true,
-    useUnifiedTopology: true
-})
+// mongoose.connect(process.env.MONGODB_URI||'mongodb+srv://ansh:anshviyogi@cluster0.syyc1cg.mongodb.net/?retryWrites=true&w=majority',{
+//     useNewUrlParser:true,
+//     useUnifiedTopology: true
+// })
 
-mongoose.connection.on('connected',()=>{
-    console.log("Mongodb connected")
+// mongoose.connection.on('connected',()=>{
+//     console.log("Mongodb connected")
+// })
+
+// Second ----------------------------
+
+
+mongoose
+.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 })
-    // .then(()=> console.log("Mongodb connected!!"))
-    // .catch((ERR)=> console.log(ERR))
+.then(() => {
+  console.log('Database connected successfully!');
+})
+.catch((err) => {
+  console.log('Error connecting with error code:', err);
+});
