7fae88f150ef4ef38d405ba44d90c81f3a0eaa0b
AnshViyogi <auni.viyogi@gmail.com>
2022-09-29 00:13:13 +0530

    assignment

--- model/db.js
+++ model/db.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://ansh:anshviyogi@cluster0.syyc1cg.mongodb.net/?retryWrites=true&w=majority')
-    .then(()=> console.log("Mongodb connected!!"))
-    .catch((ERR)=> console.log(ERR))
+mongoose.connect(process.env.MONGODB_URI||'mongodb+srv://ansh:anshviyogi@cluster0.syyc1cg.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected',()=>{
+    console.log("Mongodb connected")
+})
+    // .then(()=> console.log("Mongodb connected!!"))
+    // .catch((ERR)=> console.log(ERR))
